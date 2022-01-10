906e2ee115b1bd5632b4cd6f92aab90ef4a627d5
Aayush Mehta <aayushmehta0924@gmail.com>
2022-01-10 17:57:06 +0530

    console logging weather data

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let weather = {
     "apiKey": "7c69856704510a9bfd63c6b6d234d106",
-    fetchWeather: function() {
+    fetchWeather: function(city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=7c69856704510a9bfd63c6b6d234d106"
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
         )
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
     },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    }
 };
